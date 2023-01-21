323e3485946cd6f7222e94315843eb94fb7e1c95
mdruhaib23 <120497565+mdruhaib23@users.noreply.github.com>
2023-01-21 18:19:25 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [result, setResult] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [player, setplayer] = useState("X");
+  const [gameOn, setGameOn] = useState(true);
+  const [gameDraw, setGameDraw] = useState(false);
+
+  useEffect(() => {
+    if (checkForWinner()) {
+      setGameOn(false);
+    }
+    if (result.filter((item) => item !== "").length === 9) {
+      setGameOn(false);
+      setGameDraw(true);
+    }
+  }, [result]);
+
+  const checkForWinner = () => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let condition = 0; condition < winConditions.length; condition++) {
+      if (
+        (result[winConditions[condition][0]] === "X" &&
+          result[winConditions[condition][1]] === "X" &&
+          result[winConditions[condition][2]] === "X") ||
+        (result[winConditions[condition][0]] === "O" &&
+          result[winConditions[condition][1]] === "O" &&
+          result[winConditions[condition][2]] === "O")
+      ) {
+        console.log("Hello");
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const handleClick = (index) => {
+    if (!result[index] && gameOn) {
+      const newResult = result.map((item, thisIndex) =>
+        thisIndex === index ? player : item
+      );
+      setplayer(player === "X" ? "O" : "X");
+      setResult(newResult);
+    }
+  };
+
+  const getMessage = () => {
+    if (gameOn) {
+      return `Player ${player}'s turn`;
+    } else {
+      return gameDraw ? `Game Draw` : `Winner is ${player === "X" ? "O" : "X"}`;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      {result.map((item, index) => (
+        <div key={index} className="box" onClick={() => handleClick(index)}>
+          {item}
+        </div>
+      ))}
+      {getMessage()}
     </div>
   );
 }
-
-export default App;
